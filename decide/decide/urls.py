da2d027d43525a7d1ab3bfdb14325c9411c53713
Antonio23337 <antoniobarea233@gmail.com>
2023-12-13 18:32:17 +0100

    feat(Traducciones): Añadida traduccion de la pagina al español, frances e ingles en la cabina de votación

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
+from django.views.i18n import set_language
 
 
 schema_view = get_swagger_view(title='Decide API')
@@ -25,6 +26,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('doc/', schema_view),
     path('gateway/', include('gateway.urls')),
+    path('i18n/', set_language, name='set_language'),
+    
 ]
 
 for module in settings.MODULES:
