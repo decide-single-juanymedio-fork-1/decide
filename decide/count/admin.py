972fef807e160bb64da7ca775c9265672ff72d11
Fausto Vazquez Rodriguez <fausto4vazrod@gmail.com>
2023-12-17 02:24:04 +0100

    feat: Implementado método de recuento Sainte-Lague #21

--- decide/count/admin.py
+++ decide/count/admin.py
@@ -13,7 +13,7 @@ class ApportionmentAdmin(admin.ModelAdmin):
     list_filter = (['voting_id'])
     search_fields = (['voter_id', 'method'])
 
-    actions = ['dhondt']
+    actions = ['dhondt','sainte_lague']
 
     def dhondt(self, request, queryset):
         for apportionment in queryset:
@@ -53,5 +53,44 @@ class ApportionmentAdmin(admin.ModelAdmin):
                 )
 
     dhondt.short_description = 'D\'Hondt Method'
+
+    def sainte_lague(self, request, queryset):
+        for apportionment in queryset:
+            voting_id = apportionment.voting_id
+            try:
+                voting = Voting.objects.get(id=voting_id)
+                if apportionment.method is None:
+                    if voting.tally is not None:
+                        apportionment.sainte_lague(voting)
+                        # Mensaje de éxito
+                        self.message_user(
+                            request,
+                            ngettext(
+                                'voting %d was successfully processed.',
+                                'votings %d were successfully processed.',
+                                queryset.count()
+                            ) % queryset.count(),
+                            level='SUCCESS'
+                        )
+                    else:
+                        self.message_user(
+                            request,
+                            f"Voting with ID {voting_id} has a null 'tally' field and will be skipped.",
+                            level='WARNING'
+                        )
+                else:
+                    self.message_user(
+                        request,
+                        f"Voting with ID {voting_id} already has a counting method applied and will be skipped.",
+                        level='WARNING'
+                    )
+            except Voting.DoesNotExist:
+                self.message_user(
+                    request,
+                    f"Voting with ID {voting_id} does not exist.",
+                    level='ERROR'
+                )
+
+    sainte_lague.short_description = 'Sainte-Laguë Method'
 
 admin.site.register(Apportionment, ApportionmentAdmin)
--- decide/count/models.py
+++ decide/count/models.py
@@ -31,6 +31,23 @@ class Apportionment(models.Model):
         self.solution = formatted_solution
         self.save()
 
+    def sainte_lague(self, voting):
+        self.method = 'Sainte-Laguë'
+        postproc_dict = self.generate_postproc_dict(voting)
+        seats_count_dicc = postproc_dict.copy()
+        solution_dict = {key: 0 for key in postproc_dict.keys()}
+        seats_number = self.seats
+        for seat in range(seats_number):
+            # Actualizar seats_count_dicc dividiendo el número de votos originales entre el nuevo número de escaños asignados hasta entonces
+            seats_count_dicc = {key: postproc_dict[key] / (2*(solution_dict[key]+1) - 1) for key in seats_count_dicc.keys()}
+            # Encontrar la opción con el máximo valor en el postproc_dict
+            max_option = max(seats_count_dicc, key=seats_count_dicc.get)
+            # Incrementar el valor correspondiente en el solution_dict
+            solution_dict[max_option] += 1
+        formatted_solution = ', '.join([f'{key}: {value}' for key, value in solution_dict.items()])
+        self.solution = formatted_solution
+        self.save()
+
     class Meta:
         unique_together = (('voting_id', 'seats', 'method'),)
 
