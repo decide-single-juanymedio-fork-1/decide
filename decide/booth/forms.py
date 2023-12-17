fcfce84fb1a2f01dd61076363b5fb173b987d5b0
antsuagar <antsuagar@alum.us.es>
2023-12-18 00:39:46 +0100

    feat: Modificar usuario completado
    El usuario puede modificar su información personal mediante un formulario accesible desde la página principal.

--- decide/booth/forms.py
+++ decide/booth/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 from django.contrib.auth.models import User
 
@@ -16,3 +16,9 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class CustomUserChangeForm(UserChangeForm):
+    password = None
+    class Meta:
+        model = User
+        fields = ['username', 'email']
